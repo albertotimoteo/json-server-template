057ce40824ce0d272d13e86a1efb79b87285b360
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2023-01-05 16:09:02 -0300

    feat: first try to solve the error

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require("json-server")
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router("./tmp/db.json")
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+)
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log("JSON Server is running")
 })
 
 // Export the Server API
