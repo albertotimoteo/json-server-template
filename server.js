697dbaf90c788484bf77c8fd1dd62eb9ff82f207
Alberto Timoteo <alberto.pptimoteo@gmail.com>
2023-01-05 16:24:28 -0300

    feat: trying new approach

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server")
+const clone = require("clone")
+const data = require("./db.json")
+
+const isProductionEnv = process.env.NODE_ENV === "production"
 const server = jsonServer.create()
-const router = jsonServer.router("/tmp/db.json")
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
+  _isFake: isProductionEnv,
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-)
+
+server.use((req, res, next) => {
+  if (req.path !== "/") router.db.setState(clone(data))
+  next()
+})
+
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log("JSON Server is running")
 })
 
